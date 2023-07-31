12527c9d681926f06f204726eb35903238b2f5d0
lsuinl <su96in43@naver.com>
2023-07-31 15:47:41 +0900

    feat: 기록 메인 화면 ui 만들기

--- lib/home/screen/note_screen.dart
+++ lib/home/screen/note_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:univ_note/common/basic.dart';
 
 class NoteScreen extends StatefulWidget {
@@ -11,6 +12,195 @@ class NoteScreen extends StatefulWidget {
 class _NoteScreenState extends State<NoteScreen> {
   @override
   Widget build(BuildContext context) {
-    return Basic(paddings: 0, widgets: Text("나는노트"));
+    return Basic(
+        paddings: 0,
+        widgets: Stack(
+        children:[
+        Column(
+          children: [
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: Row(
+                  children: [
+                    InputChip(
+                      onPressed:(){
+                      },
+                        label: Text("전체보기",style: TextStyle(fontSize: 14.sp),),
+                      backgroundColor: Colors.white70,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.r)),
+                      side:  BorderSide(
+                          color: Colors.black54
+                      ),
+                    ),
+                    SizedBox(width: 10.w),
+                    InputChip(
+                      onPressed:(){},
+                      label: Text("교내활동",style: TextStyle(fontSize: 14.sp),),
+                      backgroundColor: Colors.white70,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.r)),
+                      side:  BorderSide(
+                          color: Colors.black54
+                      ),
+                    ),
+                    SizedBox(width: 10.w),
+                    InputChip(
+                      onPressed:(){},
+                      label: Text("봉사활동",style: TextStyle(fontSize: 14.sp),),
+                      backgroundColor: Colors.white70,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.r)),
+                      side:  BorderSide(
+                          color: Colors.black54
+                      ),
+                    ),
+                    SizedBox(width: 10.w)
+
+                  ],
+                )),
+            Container(height: 1, color: Colors.black),
+            Flexible(child:
+            Padding(
+                padding: EdgeInsets.all(10),
+                child:Container(
+                child: ListView(
+                  children: [       Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("교내 활동", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24.sp),),
+                        Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.r)),
+                            elevation: 5, //그림자
+                            child: InkWell(
+                              onTap: (){},
+                              child: Container(
+                                  height: 75.h,
+                                  width: MediaQuery.of(context).size.width,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(),
+                                    borderRadius: BorderRadius.circular(10.r),
+                                  ),
+                                  child: Padding(
+                                      padding: EdgeInsets.all(10),
+                                      child: Stack(
+                                        children: [
+                                          Column(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text("도서관 서포터즈 활동",style: TextStyle(fontSize: 18.sp,fontWeight: FontWeight.w600)),
+                                              Text("관리 업무를 맡으며 독서의 날 에는 ..", style:  TextStyle(fontSize: 14.sp,fontWeight: FontWeight.w500),),
+                                              Text("2023-03-05~2023-07-22",style: TextStyle(color: Colors.black54),),
+                                            ],
+                                          ),
+                                          Container(
+                                            alignment: Alignment.centerRight,
+                                            child: Icon(Icons.arrow_forward_ios),
+                                          )
+                                        ],
+                                      )
+                                  )
+
+                              ),
+                            )
+                        ),
+                        Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.r)),
+                            elevation: 5, //그림자
+                            child: InkWell(
+                              onTap: (){},
+                              child: Container(
+                                  height: 75.h,
+                                  width: MediaQuery.of(context).size.width,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(),
+                                    borderRadius: BorderRadius.circular(10.r),
+                                  ),
+                                  child: Padding(
+                                      padding: EdgeInsets.all(10),
+                                      child: Stack(
+                                        children: [
+                                          Column(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text("도서관 서포터즈 활동",style: TextStyle(fontSize: 18.sp,fontWeight: FontWeight.w600)),
+                                              Text("관리 업무를 맡으며 독서의 날 에는 ..", style:  TextStyle(fontSize: 14.sp,fontWeight: FontWeight.w500),),
+                                              Text("2023-03-05~2023-07-22",style: TextStyle(color: Colors.black54),),
+                                            ],
+                                          ),
+                                          Container(
+                                            alignment: Alignment.centerRight,
+                                            child: Icon(Icons.arrow_forward_ios),
+                                          )
+                                        ],
+                                      )
+                                  )
+
+                              ),
+                            )
+                        ),
+                        SizedBox(height: 10.h,),
+                        Text("교내 활동", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 24.sp),),
+                        Card(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.r)),
+                            elevation: 5, //그림자
+                            child: InkWell(
+                              onTap: (){},
+                              child: Container(
+                                  height: 75.h,
+                                  width: MediaQuery.of(context).size.width,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(),
+                                    borderRadius: BorderRadius.circular(10.r),
+                                  ),
+                                  child: Padding(
+                                      padding: EdgeInsets.all(10),
+                                      child: Stack(
+                                        children: [
+                                          Column(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text("도서관 서포터즈 활동",style: TextStyle(fontSize: 18.sp,fontWeight: FontWeight.w600)),
+                                              Text("관리 업무를 맡으며 독서의 날 에는 ..", style:  TextStyle(fontSize: 14.sp,fontWeight: FontWeight.w500),),
+                                              Text("2023-03-05~2023-07-22",style: TextStyle(color: Colors.black54),),
+                                            ],
+                                          ),
+                                          Container(
+                                            alignment: Alignment.centerRight,
+                                            child: Icon(Icons.arrow_forward_ios),
+                                          )
+                                        ],
+                                      )
+                                  )
+
+                              ),
+                            )
+                        ),
+                      ]
+                  )],
+    )
+    )
+            ))]),
+    Container(
+      alignment: Alignment.bottomRight,
+    child:
+Padding(
+  padding:EdgeInsets.all(20),
+    child: RawMaterialButton(
+      onPressed: (){},
+      elevation: 5.0,
+      fillColor: Colors.blue[300],
+      child: Icon(
+        Icons.edit,
+        size: 50.0,
+        color: Colors.white,
+      ),
+      padding: EdgeInsets.all(15.0),
+      shape: CircleBorder(),
+    )
+    ))
+        ]
+        )
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:univ_note/home/screen/home_screen.dart';
+import 'package:univ_note/home/screen/setting_screen.dart';
 import 'package:univ_note/start/screen/start_screen.dart';
 import 'package:univ_note/user/login/screen/login_screen.dart';
 import 'package:univ_note/user/register/screen/ending_screen.dart';
@@ -14,7 +15,7 @@ void main() {
       designSize: Size(360, 690),
       builder: (context, child) {
         return MaterialApp(
-          home: InputOutyearScreen(),
+          home: HomeScreen(),
         );
       }));
 }
